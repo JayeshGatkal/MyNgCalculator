9781ee180e1b890b50f601ef4a8d2a4d2a2f9691
Jayesh <jayeshgatkal@outlook.com>
2020-05-02 13:21:34 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'calculator-app';
+  num1: number;
+  num2: number;
+  result: number;
+  result_color: string;
+
+  add() {
+    this.result = this.num1 + this.num2;
+    this.result_color = (this.result <= 0 || isNaN(this.result)) ? 'negative' : 'positive';
+  }
+
+  substract() {
+    this.result = this.num1 - this.num2;
+    this.result_color = (this.result <= 0 || isNaN(this.result)) ? 'negative' : 'positive';
+  }
+
+  multiply() {
+    this.result = this.num1 * this.num2;
+    this.result_color = (this.result <= 0 || isNaN(this.result)) ? 'negative' : 'positive';
+  }
+
+  divide() {
+    this.result = this.num1 % this.num2;
+    this.result_color = (this.result <= 0 || isNaN(this.result)) ? 'negative' : 'positive';
+  }
+}
